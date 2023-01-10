6df46d3a6ba00cfb52dc6ad3a6bed09a4c116032
Brunogomes97 <brunogxy10@gmail.com>
2023-01-10 13:24:22 -0300

    first commit

---
+++ src/modules/cars/useCases/createSpecification/index.ts
@@ -0,0 +1,14 @@
+import { SpecificationsRepository } from "../../repositories/implementations/SpecificationsRepository";
+import { CreateSpecificationController } from "./CreateSpecificationController";
+import { CreateSpecificationUseCase } from "./CreateSpecificationUseCase";
+
+const specificationsRepository = new SpecificationsRepository();
+const createCategoryUseCase = new CreateSpecificationUseCase(
+  specificationsRepository
+);
+
+const createSpecificationController = new CreateSpecificationController(
+  createCategoryUseCase
+);
+
+export { createSpecificationController };
